00c9d6a47f45ebfbd5435a524ce2d248b11bc872
Valentine Gamma <gamma.dashboard@gmail.com>
2025-06-24 17:11:05 +0300

    vesion 1.3.1 beta

---
+++ scripts/links.js
@@ -0,0 +1,8 @@
+// document.getElementById("").href = "#about";
+
+// const navLinks = document.querySelectorAll(".nav-link-item");
+// const linkIDs = ["#about", "#portfolio", "#projects", "#services", "#contact", "#blog"];
+
+// navLinks.forEach((link, index) => {
+//   link.href = sectionIds[index];
+// });
